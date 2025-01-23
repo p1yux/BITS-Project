c94da3f8ef069397bffd49fc19053f4431017081
p1yux <soulpiyush09@gmail.com>
2025-01-23 20:37:04 +0530

    ...

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google'
 import { AudioProvider } from "@/context/AudioContext";
 import AudioControl from "@/components/custom/AudioControl";
 import "./globals.css";
 import { AuthProvider } from '@/contexts/AuthContext';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "BITS 2025 - Technology Summit",
@@ -21,10 +13,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={inter.className}>
+      <body>
         <AuthProvider>
           <AudioProvider>
             {children}
